9747cc758d33555dada27a0ca5a8cae669cf06f1
Plumeyu-hub <1404300825@qq.com>
2020-08-05 18:11:59 +0800

    修改HelpActivit页面的标题栏封装问题

--- app/src/main/java/com/snxun/book/ui/my/HelpActivity.java
+++ app/src/main/java/com/snxun/book/ui/my/HelpActivity.java
@@ -1,40 +1,32 @@
 package com.snxun.book.ui.my;
 
-import android.view.View;
-import android.widget.ImageView;
-
 import com.snxun.book.R;
 import com.snxun.book.base.BaseActivity;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class HelpActivity extends BaseActivity {
-    /**
-     * 返回按钮
-     */
-    @BindView(R.id.help_back_btn)
-    ImageView mHelpBackBtn;
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_help;
     }
 
-
     @Override
     protected void findViews() {
         ButterKnife.bind(this);
+        initTitleLayout();
+    }
+
+    private void initTitleLayout() {
+        showTitleBar();
+        getTitleLayout().setTitleName(R.string.drawer_help);
     }
 
     @Override
-    protected void setListeners() {
-        super.setListeners();
-        mHelpBackBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+    protected void clickBackBtn() {
+        super.clickBackBtn();
+        finish();
     }
+
 }
