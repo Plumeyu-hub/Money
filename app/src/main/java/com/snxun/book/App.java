e0d726fc80af68d801c657f0ea570fc800a13138
Plumeyu-hub <1404300825@qq.com>
2020-08-14 18:09:44 +0800

    适配安卓6.0

--- app/src/main/java/com/snxun/book/App.java
+++ app/src/main/java/com/snxun/book/App.java
@@ -3,7 +3,10 @@ package com.snxun.book;
 import android.app.Application;
 import android.content.Context;
 
+import com.lodz.android.component.event.ActivityFinishEvent;
 import com.snxun.book.greendaolib.GreenDaoManager;
+
+import org.greenrobot.eventbus.EventBus;
 
 /**
  * @author zhouL
@@ -24,8 +27,25 @@ public class App extends Application {
         initDb(getApplicationContext());
     }
 
-    /** 初始化数据库 */
+    /**
+     * 初始化数据库
+     */
     private void initDb(Context context) {
         GreenDaoManager.get().init(context);
+    }
+
+    /**
+     * 退出app
+     */
+    public void exit() {
+        System.exit(0);
+    }
+
+    /**
+     * 关闭所有Activity
+     */
+    public void finishActivities() {
+        // 发送关闭事件
+        EventBus.getDefault().post(new ActivityFinishEvent());
     }
 }
--- app/src/main/java/com/snxun/book/ui/my/ExportActivity.java
+++ app/src/main/java/com/snxun/book/ui/my/ExportActivity.java
@@ -1,13 +1,22 @@
 package com.snxun.book.ui.my;
 
+import android.Manifest;
 import android.app.AlertDialog.Builder;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.lodz.android.core.utils.AppUtils;
+import com.lodz.android.core.utils.ToastUtils;
+import com.lodz.android.core.utils.UiHandler;
+import com.snxun.book.App;
 import com.snxun.book.R;
 import com.snxun.book.base.BaseActivity;
 import com.snxun.book.db.DbFactory;
@@ -20,7 +29,17 @@ import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
+/**
+ * 在需要动态申请权限的类上添加 @RuntimePermissions 的注解
+ */
+@RuntimePermissions
 public class ExportActivity extends BaseActivity {
 
     /**
@@ -72,6 +91,102 @@ public class ExportActivity extends BaseActivity {
     @Override
     protected void initData() {
         super.initData();
+        UiHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {// 6.0以上的手机对权限进行动态申请
+                    //申请权限
+                    ExportActivityPermissionsDispatcher.requestPermissionWithPermissionCheck(ExportActivity.this);
+                } else {
+                    init();
+                }
+            }
+        }, 1000);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        ExportActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);//将回调交给代理类处理
+    }
+
+    /**
+     * 权限申请成功 请求许可
+     */
+    //单一权限
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    //多权限
+    //    @NeedsPermission({
+    //            Manifest.permission.READ_PHONE_STATE,// 手机状态
+    //            Manifest.permission.WRITE_EXTERNAL_STORAGE,// 存储卡读写
+    //            Manifest.permission.READ_EXTERNAL_STORAGE,// 存储卡读写
+    //    })
+    protected void requestPermission() {
+        //        if (!AppUtils.isPermissionGranted(getContext(), Manifest.permission.READ_PHONE_STATE)){
+        //            return;
+        //        }
+        if (!AppUtils.isPermissionGranted(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            return;
+        }
+        //        if (!AppUtils.isPermissionGranted(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)){
+        //            return;
+        //        }
+        init();
+    }
+
+    /**
+     * 被拒绝
+     */
+    @OnPermissionDenied(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    protected void onDenied() {
+        ExportActivityPermissionsDispatcher.requestPermissionWithPermissionCheck(this);//申请权限
+    }
+
+    /** 用户拒绝后再次申请前告知用户为什么需要该权限 */
+    @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    protected void showRationaleBeforeRequest(PermissionRequest request) {
+        request.proceed();//请求权限
+    }
+
+    /** 被拒绝并且勾选了不再提醒 */
+    @OnNeverAskAgain(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    protected void onNeverAskAgain() {
+        ToastUtils.showShort(getContext(), "请在权限页面打开应用所有权限");
+        showPermissionCheckDialog();
+        AppUtils.jumpAppDetailSetting(this);
+    }
+
+    /** 显示权限核对弹框 */
+    private void showPermissionCheckDialog() {
+        CheckDialog dialog = new CheckDialog(getContext());
+        dialog.setContentMsg("请确认已打开该应用使用的所有权限");
+        dialog.setPositiveText("已确认", new CheckDialog.Listener() {
+            @Override
+            public void onClick(Dialog dialog) {
+                ExportActivityPermissionsDispatcher.requestPermissionWithPermissionCheck(ExportActivity.this);//申请权限
+                dialog.dismiss();
+            }
+        });
+        dialog.setNegativeText("再次确认", new CheckDialog.Listener() {
+            @Override
+            public void onClick(Dialog dialog) {
+                AppUtils.jumpAppDetailSetting(getContext());
+            }
+        });
+        //设置点击Dialog以外的区域时Dialog不消失
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                ToastUtils.showShort(getContext(),"请在设置中打开权限后重启应用");
+                App.get().exit();
+            }
+        });
+        dialog.show();
+    }
+
+
+    private void init() {
         showUserInfo();
     }
 
